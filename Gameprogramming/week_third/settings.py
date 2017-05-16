5f5f89ee12f9fcd0fadfbc8489561bd927677bb6
June <korca02220@naver.com>
2017-05-16 22:49:56 +0900

    commit

--- Gameprogramming/week_third/settings.py
+++ Gameprogramming/week_third/settings.py
@@ -1,6 +1,6 @@
 #base properties
-BACKGORUND = 'Image\city_image.jpg'
-BOX = 'Image\Moon.png'
+BACKGROUND = 'Image\city_image.jpg'
+BACKGROUND2 = 'Image\city_image2.jpg'
 TITLE = "My game"
 WIDTH = 512
 HEIGHT = 600
@@ -11,14 +11,14 @@ FONT_NAME = 'arial'
 PLAYER_ACC = 0.65
 PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.5
-PLAYER_JUMP = 12.5
+PLAYER_JUMP = 14
 
 # Starting platforms
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
                 (125, HEIGHT - 350, 100, 20),
-                (350, 300, 100, 20),
-                (175, 200, 50, 20)] #total 5 List
+                (350, 400, 100, 20),
+                (175, 3000, 50, 20)] #total 5 List
 
 #define colors
 WHITE = (255, 255, 255)
@@ -38,5 +38,3 @@ MUSIC_DICT = {'Catching' :'Music\Catching the beat.mp3',
               'Summer' : 'Music\Summer_Break.mp3'
               }
 MUSIC_LIST = ['Catching', 'CountingStar', 'Fight', 'Flaming', 'Start', 'Summer']
-
-
